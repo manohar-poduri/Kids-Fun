aef4b4f49cfe74ccd9883337a74b13e97edcfd14
Manohar - IT man <poduri.manohar46@gmal.com>
2020-09-09 18:03:50 +0530

    Add new Activity for game end

--- app/src/main/java/com/example/lionortiger/FishGame/GameOverActivity.java
+++ app/src/main/java/com/example/lionortiger/FishGame/GameOverActivity.java
@@ -2,15 +2,31 @@ package com.example.lionortiger.FishGame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.lionortiger.MainActivity;
 import com.example.lionortiger.R;
 
 public class GameOverActivity extends AppCompatActivity {
+
+    private Button startGameAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        startGameAgain = findViewById(R.id.play_again_btn);
+
+        startGameAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GameOverActivity.this, FishActivity.class));
+            }
+        });
+
     }
 }
